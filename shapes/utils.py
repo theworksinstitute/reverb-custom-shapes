57e7f251f5becb3395c1c2fd74eaf60c0083c327
ia rigby <marvinzem@gmail.com>
2022-10-10 17:24:28 +0400

    fix the path issue

--- shapes/utils.py
+++ shapes/utils.py
@@ -1,8 +1,11 @@
 import re
+import os
 
 
-def read_vertices_from_file(file_name):
-	with open(file_name, 'r') as file:
+def read_vertices_from_file(path):
+	if 'data' in os.getcwd():
+		path = os.path.join(os.getcwd(), 'scripts/reverb-custom-shapes', path)
+	with open(path, 'r') as file:
 		contents = file.readlines()
 		return list(map(convert_line_to_list, contents))
 
